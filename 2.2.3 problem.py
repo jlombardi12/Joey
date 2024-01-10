3836f94ead2517f7ae28c5f955857646475e7573
jlombardi12 <84484478+jlombardi12@users.noreply.github.com>
2024-01-10 14:29:34 -0500

    initial

---
+++ 2.2.3 problem.py
@@ -0,0 +1,14 @@
+from datetime import date
+my_var1 = True
+my_var2 = date.today()
+my_var3 = my_var2.day
+my_var4 = 0.5
+
+print(type(my_var1))
+print(my_var1)
+print(type(my_var2))
+print(my_var2)
+print(type(my_var3))
+print(my_var3)
+print(type(my_var4))
+print(my_var4)
